e3c63349d00c599ca6318075ee4d0ae3769315cf
Mehnaaz Asad <mehnaaz.asad@vanderbilt.edu>
2018-09-06 22:04:28 -0500

    Using functions for everything as well as argparse

--- ECO_Vishnu_mock_catalogues/scripts/populate_mock.py
+++ ECO_Vishnu_mock_catalogues/scripts/populate_mock.py
@@ -6,33 +6,29 @@ Created on Mon Sep  3 08:55:51 2018
 @author: asadm2
 """
 
-from cosmo_utils.mock_catalogues.shmr_funcs import Behroozi_relation
 from halotools.empirical_models import PrebuiltSubhaloModelFactory
 from halotools.sim_manager import UserSuppliedHaloCatalog
 from halotools.utils import broadcast_host_halo_property
-from cosmo_utils.utils.stats_funcs import Stats_one_arr
 from halotools.sim_manager import CachedHaloCatalog
 from progressbar import ProgressBar
-import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
+import argparse
+import os
 
-cols_to_use = [1,5,6,10,11,12,17,18,19,20,21,22,39,60,61,62,63]
-col_names = ['halo_id','halo_pid','halo_upid','halo_mvir','halo_rvir',\
-             'halo_rs','halo_x','halo_y','halo_z','halo_vx','halo_vy',\
-             'halo_vz','halo_m200b','halo_macc','halo_mpeak','halo_vacc',\
-             'halo_vpeak']
-rockstar_table = pd.read_table('hlist_0.98169.list.gz',delimiter='\s+',\
+def create_halocat_from_snapshot(col_ids,col_names,snapshot_file,\
+                                 processing_notes):
+    rockstar_table = pd.read_table(snapshot_file,delimiter='\s+',\
                                compression='gzip',comment='#',\
-                               usecols=cols_to_use,names=col_names)
+                               usecols=col_ids,names=col_names)
 
-rockstar_table.halo_rvir = rockstar_table.halo_rvir/1000 
-rockstar_table.halo_rs = rockstar_table.halo_rs/1000
+    rockstar_table.halo_rvir = rockstar_table.halo_rvir/1000 
+    rockstar_table.halo_rs = rockstar_table.halo_rs/1000
+    
+    redshift = 0.0186
+    Lbox, particle_mass = 130,3.215e7
 
-redshift = 0.0186
-Lbox, particle_mass = 130,3.215e7
-
-halocat = UserSuppliedHaloCatalog(redshift=redshift,\
+    halocat = UserSuppliedHaloCatalog(redshift=redshift,\
                                   halo_rvir=rockstar_table.halo_rvir.values,\
                                   halo_vx=rockstar_table.halo_vx.values,\
                                   halo_vy=rockstar_table.halo_vy.values,\
@@ -52,71 +48,132 @@ halocat = UserSuppliedHaloCatalog(redshift=redshift,\
                                   halo_vacc=rockstar_table.halo_vacc.values,\
                                   halo_vpeak=rockstar_table.halo_vpeak.values)
 
-halocat_file_path = '/home/asadm2/.astropy/cache/halotools/halo_catalogs'\
-                    '/bolshoi/rockstar/bolshoi_test_v1.hdf5'
+    halocat_file_path = '/home/asadm2/.astropy/cache/halotools/halo_catalogs'\
+                    '/vishnu/rockstar/vishnu_rockstar.hdf5'
 
-halocat.add_halocat_to_cache(halocat_file_path,simname='bolshoi',\
-                             halo_finder='rockstar',\
-                             version_name='my_rockstar_catalog_v1.0',\
-                             processing_notes='Positions, IDs (HID,PID,UPID),'\
-                             'velocities (vpeak and vacc), radius (rvir,rs)'\
-                             'and mass information extracted'\
-                             '(mvir,macc,m200,mpeak) from original snapshot'\
-                             ,overwrite=True,redshift='0.0186')
+    halocat.add_halocat_to_cache(halocat_file_path,simname='vishnu',\
+                                 halo_finder='rockstar',\
+                                 version_name='vishnu_rockstar_ECO_v1.0',\
+                                 processing_notes=processing_notes,\
+                                 overwrite=True,redshift='0.0186')
+    return halocat_file_path
 
-halocat = CachedHaloCatalog(fname=halocat_file_path, update_cached_fname=True)
-model = PrebuiltSubhaloModelFactory('behroozi10',redshift=0.0186,\
-                                    prim_haloprop_key='halo_macc') 
-model.populate_mock(halocat)
+def access_halocat(halocat_file_path):
+    halocat = CachedHaloCatalog(fname=halocat_file_path,\
+                                update_cached_fname=True)
+    broadcast_host_halo_property(halocat.halo_table,'halo_macc')
+    return halocat
 
-broadcast_host_halo_property(halocat.halo_table, 'halo_macc')
+def populate_mock(key,halocat):
+    model = PrebuiltSubhaloModelFactory('behroozi10',redshift=0.0186,\
+                                        prim_haloprop_key=key) 
+    model.populate_mock(halocat)
+    return model
 
-halo_pd = halocat.halo_table.to_pandas()
-gal_pd = model.mock.galaxy_table.to_pandas()
+def create_temp_hdf5(halo_table,gal_table):
+    halo_table_fname = 'halo_table_temp.hdf5'
+    galaxy_table_fname = 'galaxy_table_temp.hdf5'
+    halo_table.write(halo_table_fname,path='updated_data',\
+                     compression=True)
+    gal_table.write(galaxy_table_fname,path='updated_data',\
+                     compression=True)
+    return halo_table_fname,galaxy_table_fname
+    
+def merge_halocat_galcat(halo_catalog_fname,galaxy_catalog_fname):
+    halo_pd = pd.read_hdf(halo_catalog_fname)
+    gal_pd = pd.read_hdf(galaxy_catalog_fname)
+    
+    cols_to_use = list(set(halo_pd.columns) - set(gal_pd.columns))
+    cols_to_use.append('halo_id')
+    
+    halocat_galcat_merged = halo_pd[cols_to_use].merge(gal_pd,on='halo_id')
+    return halocat_galcat_merged
 
-cols_to_use = list(set(halo_pd.columns) - set(gal_pd.columns))
-cols_to_use.append('halo_id')
+def centrals_satellites_flag(catalog):
+    C_S = []
+    pbar = ProgressBar()
+    
+    for idx in pbar(range(7450441)):
+        if catalog['halo_hostid'][idx] == catalog['halo_id'][idx]:
+            C_S.append(1)
+        else:
+            C_S.append(0)
+    
+    C_S = np.array(C_S)
+    catalog['C_S'] = C_S
+    return catalog
+    
+def args_parser():
+    print('Parsing in progress')
+    parser = argparse.ArgumentParser()
+    parser.add_argument('populate_mock_key',type=str,help='Halo mass type to '\
+                        'populate mocks using (halo_macc or halo_mvir)')
+    parser.add_argument('-halocat_from_snapshot',type=bool, \
+                        help='True if creating halocat from snapshot.'\
+                        'False if accessing stored halo catalog.'\
+                        'Default is False.',default=False)
+    parser.add_argument('-column_ids',nargs='+',type=int,help='Column IDs to '\
+                        'extract from snapshot',default=None)
+    parser.add_argument('-column_names',nargs='+',type=str,help='Names of '\
+                        'columns to extract',default=None)
+    parser.add_argument('-processing_notes',type=str,help='Note to add to '\
+                        'halo catalog cache. Must be one string.',default=None)
+    args = parser.parse_args()
+    return args
 
-halocat_galcat_merged = halo_pd[cols_to_use].merge(gal_pd,on='halo_id')
+def main(args):
+    cols_to_use = [1,5,6,10,11,12,17,18,19,20,21,22,39,60,61,62,63]
+    col_names = ['halo_id','halo_pid','halo_upid','halo_mvir','halo_rvir',\
+             'halo_rs','halo_x','halo_y','halo_z','halo_vx','halo_vy',\
+             'halo_vz','halo_m200b','halo_macc','halo_mpeak','halo_vacc',\
+             'halo_vpeak']
+    snapshot_file = 'hlist_0.98169.list.gz'
+    processing_notes = 'Positions, IDs (HID,PID,UPID),velocities '\
+                       '(vpeak and vacc), radius (rvir,rs) and mass '\
+                       'information extracted (mvir,macc,m200,mpeak) '\
+                       'from original snapshot'
+                       
 
-C_S = []
-pbar = ProgressBar()
-
-for idx in pbar(range(7450441)):
-    if halocat_galcat_merged['halo_hostid'][idx] == halocat_galcat_merged\
-    ['halo_id'][idx]:
-        C_S.append(1)
-    else:
-        C_S.append(0)
-
-C_S = np.array(C_S)
-halocat_galcat_merged['C_S'] = C_S
-
-
-cens = halocat_galcat_merged.loc[halocat_galcat_merged.C_S.values == 1]
-sats = halocat_galcat_merged.loc[halocat_galcat_merged.C_S.values == 0]
-
-stats_cens = Stats_one_arr(np.log10(cens.halo_mvir_host_halo.values),\
-                           np.log10(cens.stellar_mass.values))
-
-## Overplotting Behroozi 2013 relation for centrals
-log_mstar_arr = np.linspace(halocat_galcat_merged.stellar_mass.values.min(),\
-                          halocat_galcat_merged.stellar_mass.values.max(),100)
-log_mstar_arr = np.log10(log_mstar_arr)
-log_halo_mass_behroozi = Behroozi_relation(log_mstar_arr,z=0.0186)
-
-fig1 = plt.figure()
-plt.scatter(np.log10(sats.halo_mvir_host_halo.values),\
-            np.log10(sats.stellar_mass.values),color='g',label='Satellites')
-plt.plot(log_halo_mass_behroozi,log_mstar_arr,'-k',label='Behroozi 2013')
-plt.errorbar(stats_cens[0],stats_cens[1],yerr=stats_cens[2],color='r',\
-             label='Centrals')
-plt.legend(loc='best')
-plt.xlabel('Halo mass')
-plt.ylabel('Stellar mass')
-plt.show()
-
+    if args.halocat_from_snapshot:
+        for val in args.column_ids:
+            cols_to_use.append(val)
+        for name in args.column_names:
+            col_names.append(name)
+        processing_notes+=args.processing_notes
         
+        print('Creating halo catalog from snapshot')
+        halocat_file_path = create_halocat_from_snapshot(cols_to_use,\
+                                                         col_names,\
+                                                         snapshot_file,\
+                                                         processing_notes)
+    
+    else:
+        halocat_file_path = '/home/asadm2/.astropy/cache/halotools/'\
+                            'halo_catalogs/vishnu/rockstar/vishnu_rockstar.hdf5'
+    
+    print('Accessing stored halo catalog')
+    halocat = access_halocat(halocat_file_path)
+    print('Populating mock using {0}'.format(args.populate_mock_key))
+    model = populate_mock(args.populate_mock_key,halocat)
 
-
-
+    #Bypass big and little endian compiler issues when converting astropy 
+    #tables to pandas dataframes directly
+    print('Making temporary copies of halo and galaxy catalogs')
+    halo_catalog_fname,galaxy_catalog_fname = \
+    create_temp_hdf5(halocat.halo_table,model.mock.galaxy_table)
+    print('Merging both catalogs')
+    halocat_galcat_merged = merge_halocat_galcat(halo_catalog_fname,\
+                                                 galaxy_catalog_fname)
+    print('Adding centrals/satellites flag')
+    halocat_galcat_merged = centrals_satellites_flag(halocat_galcat_merged)
+    print('Saving galaxy catalog as hdf5 file')
+    halocat_galcat_merged.to_hdf('../halo_gal_Vishnu_Rockstar_{0}.h5'.format\
+                                 (args.populate_mock_key.split('_')[1]),\
+                                 key='halocat_galcat_merged',mode='w')
+    os.remove(halo_catalog_fname)
+    os.remove(galaxy_catalog_fname)
+    
+# Main function
+if __name__ == '__main__':
+    args = args_parser()
+    main(args)
