b9dbf58f6dc2df845188b8599c0c943a244df599
Mehnaaz Asad <mehnaaz.asad@vanderbilt.edu>
2018-09-20 10:02:11 -0500

    Separated part where mock is constructed after SHAM

---
+++ ECO_Vishnu_mock_catalogues/scripts/SHAM.py
@@ -0,0 +1,236 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug  3 14:10:35 2018
+
+@author: asadm2
+"""
+import matplotlib as mpl
+mpl.use('agg')
+from scipy.optimize import curve_fit
+from progressbar import ProgressBar
+from multiprocessing import Pool 
+import matplotlib.pyplot as plt
+from scipy import interpolate
+from matplotlib import rc
+import seaborn as sns
+import pandas as pd
+import numpy as np
+import argparse
+import sympy
+import math
+import csv
+
+###Formatting for plots and animation
+rc('font',**{'family':'sans-serif','sans-serif':['Helvetica']},size=15)
+rc('text', usetex=True)
+
+### Schechter function
+def Schechter_func(M,phi_star,M_star,alpha):
+    const = 0.4*np.log(10)*phi_star
+    first_exp_term = 10**(0.4*(alpha+1)*(M_star-M))
+    second_exp_term = np.exp(-10**(0.4*(M_star-M)))
+    return const*first_exp_term*second_exp_term
+
+### Differential method
+def diff_num_dens(data,nbins,weights,volume,mag_bool):
+    if weights is None:
+        weights = np.ones(len(data))
+    else:
+        weights = np.array(weights)
+    #Unnormalized histogram and bin edges
+    freq,edg = np.histogram(data,bins=nbins,weights=weights)
+    print(freq)
+    bin_centers = 0.5*(edg[1:]+edg[:-1])
+    bin_width = edg[1] - edg[0]
+    err_poiss = np.sqrt(freq)/(volume*bin_width)
+    n_diff = freq/(volume*bin_width)
+    return bin_centers,edg,n_diff,err_poiss
+
+### Given data calculate how many bins should be used
+def num_bins(data_arr):
+    q75, q25 = np.percentile(data_arr, [75 ,25])
+    iqr = q75 - q25
+    num_points = len(data_arr)
+    h =2*iqr*(num_points**(-1/3))
+    n_bins = math.ceil((max(data_arr)-min(data_arr))/h) #Round up number   
+    return n_bins
+
+def velocity_func(r,r_s,r_vir,M_vir):
+    G = 4.302*10**-3 #pc M_sun^-1 (km/s)^2
+    const = np.sqrt(G*M_vir/r_vir)
+    num = np.log(1+(r/r_s)) + ((r/r_s)/(1+(r/r_s)))
+    denom = (r/r_vir)*((np.log(1+(r_vir/r_s))) + ((r_vir/r_s)/(1+(r_vir/r_s))))
+    result = const*(np.sqrt(num/denom))
+    return result
+
+### Data file used
+eco_obs_catalog = pd.read_csv('../data/gal_Lr_Mb_Re.txt',\
+                              delimiter='\s+',header=None,skiprows=2,\
+                              names=['M_r','logmbary','Re'])
+
+Mr_all = eco_obs_catalog.M_r.values
+Mr_unique = np.unique(eco_obs_catalog.M_r.values)
+v_eco = 192351.36 #Volume of ECO with buffer in (Mpc/h)^3
+min_Mr = min(Mr_all)
+max_Mr = max(Mr_all)
+
+
+### Using SF to fit data between -17.33 and -23.5 and extrapolating both ends
+Mr_cut = [value for value in Mr_all if value <= -17.33 and value >= -23.5]
+max_Mr_cut = max(Mr_cut)
+min_Mr_cut = min(Mr_cut)
+nbins = 25 #better chi-squared than 35 from num_bins function
+
+### Calculate differential number density using function
+bin_centers_cut,bin_edges_cut,n_Mr_cut,err_poiss_cut = diff_num_dens(Mr_cut,\
+                                                                     nbins,\
+                                                                     None,\
+                                                                     v_eco,\
+                                                                     mag_bool=\
+                                                                     True)
+
+p0 = [10**-2,-20,-1.2] #initial guess for phi_star,M_star,alpha
+
+### Curve fit using Schechter function defined
+params_noextrap,pcov = curve_fit(Schechter_func,bin_centers_cut,n_Mr_cut,p0,\
+                                 sigma=err_poiss_cut,absolute_sigma=True,\
+                                 maxfev=20000,method='lm')
+
+### Best fit parameters from curve fit
+Phi_star_bestfit = params_noextrap[0]
+M_star_bestfit = params_noextrap[1]
+Alpha_bestfit = params_noextrap[2]
+
+### Fitting data between -17.33 and -23.5
+fit_noextrap = Schechter_func(np.linspace(min_Mr_cut,max_Mr_cut),\
+                        Phi_star_bestfit,M_star_bestfit,\
+                        Alpha_bestfit)
+
+### Extrapolations on both ends
+fit_extrap_dim = Schechter_func(np.linspace(max_Mr_cut,max_Mr),\
+                      Phi_star_bestfit,M_star_bestfit,\
+                      Alpha_bestfit)
+
+fit_extrap_bright = Schechter_func(np.linspace(min_Mr,min_Mr_cut),\
+                      Phi_star_bestfit,M_star_bestfit,\
+                      Alpha_bestfit)
+
+### Percentage fractional difference
+data_fit_frac = []
+for index,value in enumerate(bin_centers_cut):
+    n_data = n_Mr_cut[index]
+    n_fit = Schechter_func(value,Phi_star_bestfit,M_star_bestfit,\
+                     Alpha_bestfit)
+    frac_diff = np.abs(((n_fit-n_data)/n_data)*100)
+    data_fit_frac.append(frac_diff)
+    
+### Chi_squared 
+model_fit = Schechter_func(bin_centers_cut,\
+                        Phi_star_bestfit,M_star_bestfit,\
+                        Alpha_bestfit)
+chi2 = 0
+for i in range(len(bin_centers_cut)):
+    chi2_i = ((n_Mr_cut[i]-model_fit[i])**2)/((err_poiss_cut[i])**2)
+    chi2 += chi2_i
+  
+#################################PLOTS#########################################
+### Plotting data + fit and extrapolation
+fig,(ax1,ax2) = plt.subplots(2,1,sharex=True,sharey=False,figsize=(20,20),\
+     gridspec_kw = {'height_ratios':[8,2]})
+ax1.set_yscale('log')
+ax1.axvline(-17.33,ls='--',c='g',label='ECO/RESOLVE-A')
+ax1.axvline(-17,ls='--',c='r',label='RESOLVE-B')
+ax1.invert_xaxis()
+### Data + errorbars
+ax1.errorbar(bin_centers_cut,n_Mr_cut,yerr=err_poiss_cut,fmt="ks--",ls='None',\
+             elinewidth=0.5,ecolor='k',capsize=5,capthick=0.5,markersize=4,\
+             label='data between {0} and -17.33'.format\
+             (np.round(min_Mr_cut,1)))
+### Data fit
+ax1.plot(np.linspace(min_Mr_cut,max_Mr_cut),fit_noextrap,'--k',\
+         label=r'$\mathrm\chi^{2} = %s$' %(np.round(chi2,2)))
+### Extrapolation on dim end
+ax1.plot(np.linspace(max_Mr_cut,max_Mr),fit_extrap_dim,'--y',label='extrap')
+### Extrapolation on bright end
+ax1.plot(np.linspace(min_Mr,min_Mr_cut),fit_extrap_bright,'--y')
+
+ax1.set_ylabel(r'$\mathrm{[\frac{dn}{dmag}]}/[\mathrm{h}^{3}\mathrm{Mpc}^{-3}'
+                '\mathrm{mag}^{-1}]$')
+ax1.legend(loc='upper right', prop={'size': 12})
+
+### Percentage fractional difference
+ax2.scatter(bin_centers_cut,data_fit_frac,c='k')
+ax2.set_xlabel(r'$M_{r}$')
+ax2.set_ylabel(r'$\mathrm{[\frac{model-data}{data}]} \%$')
+fig.tight_layout()
+fig.show()
+###############################################################################
+### Inverting the Schechter function using sympy so that it will return 
+### magnitudes given a number density
+
+### Make all parameters symbols
+n,M,phi_star,M_star,alpha = sympy.symbols('n,M,phi_star,M_star,alpha')
+const = 0.4*sympy.log(10)*phi_star
+first_exp_term = 10**(0.4*(alpha+1)*(M_star-M))
+second_exp_term = sympy.exp(-10**(0.4*(M_star-M)))
+### Make expression that will be an input for sympy.solve
+expr = (const*first_exp_term*second_exp_term)-n
+### Get schechter function in terms of M
+result = sympy.solve(expr,M,quick=True)
+### Make result a lambda function so you can pass parameter and variable values
+result_func = sympy.lambdify((n,phi_star,M_star,alpha),result,\
+                             modules=["sympy"])
+
+### Get parameter values and change variable names because the original ones
+### are now symbols
+phi_star_num = params_noextrap[0]
+M_star_num = params_noextrap[1]
+alpha_num = params_noextrap[2]
+### Pick a magnitude range
+M_num = np.linspace(-23.5,-17.33,200)
+### Calculate n given range of M values
+n_num = Schechter_func(M_num,phi_star_num,M_star_num,alpha_num)
+### Given the n values just calculated use the lambda function in terms of n to 
+### test whether you get back the same magnitude values as M_num
+M_test = [result_func(val,phi_star_num,M_star_num,alpha_num) for val in n_num]
+### Plot both to make sure they overlap
+#fig2 = plt.figure(figsize=(10,10))
+#plt.scatter(M_num,n_num,c='b',label='n given M',s=15)
+#plt.scatter(M_test,n_num,c='r',label='M given n',s=5)
+#plt.legend(loc='best')
+#plt.show()
+
+
+### Halo data
+halocat_galcat_merged = pd.read_hdf('../halo_gal_Vishnu_Rockstar_macc.h5',\
+                                    key='halocat_galcat_merged')
+v_sim = 130**3 #(Mpc/h)^3
+vpeak = halocat_galcat_merged.halo_vpeak.values
+nbins = num_bins(vpeak)
+bin_centers_vpeak,bin_edges_vpeak,n_vpeak,err_poiss = diff_num_dens(vpeak,\
+                                                                    nbins,\
+                                                                    None,\
+                                                                    v_sim,\
+                                                                    mag_bool=\
+                                                                    False)
+
+f_h = interpolate.InterpolatedUnivariateSpline(bin_centers_vpeak,n_vpeak)
+
+pbar = ProgressBar(maxval=len(vpeak))
+n_vpeak_arr = [f_h(val) for val in pbar(vpeak)]
+pbar = ProgressBar(maxval=len(n_vpeak_arr))
+
+### Parallelising SHAM
+def resultfunc_helper(args):
+    return result_func(*args)
+job_args = [(val, phi_star_num,M_star_num,alpha_num) for val in pbar\
+            (n_vpeak_arr)]
+pool = Pool(processes=24)
+halo_Mr_sham = pool.map(resultfunc_helper,job_args)
+halo_Mr_sham = np.ndarray.flatten(np.array(halo_Mr_sham))
+
+### Writing vpeak and Mr values to text file
+with open('../SHAM_parallel.csv', 'w') as f:
+    writer = csv.writer(f, delimiter='\t')
+    writer.writerows(zip(vpeak,halo_Mr_sham))
