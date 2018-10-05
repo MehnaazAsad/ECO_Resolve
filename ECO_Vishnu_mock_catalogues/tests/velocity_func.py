a911506c200d43bda27e168d2d31022ad57d955a
Mehnaaz Asad <mehnaaz.asad@vanderbilt.edu>
2018-10-04 23:07:58 -0500

    Added edgecolor

--- ECO_Vishnu_mock_catalogues/tests/velocity_func.py
+++ ECO_Vishnu_mock_catalogues/tests/velocity_func.py
@@ -115,7 +115,7 @@ plt.plot(Stats_one_arr_vre_sats[0],Stats_one_arr_vre_sats[1],color='b',\
             label='v_re satellites')
 plt.fill_between(Stats_one_arr_vre_sats[0],Stats_one_arr_vre_sats[1]+\
                  Stats_one_arr_vre_sats[2],Stats_one_arr_vre_sats[1]-\
-                 Stats_one_arr_vre_sats[2],color='b',alpha=0.4)
+                 Stats_one_arr_vre_sats[2],color='b',alpha=0.4,edgecolor='k')
 plt.xlabel(r'$log_{10}\ R_{e}\ $[kpc]')
 plt.ylabel(r'$log_{10}\ v\ $[km/s]')
 plt.legend(loc='best')
@@ -130,7 +130,7 @@ plt.plot(Stats_one_arr_vre_cens[0],Stats_one_arr_vre_cens[1],color='r',\
             label='v_re centrals')
 plt.fill_between(Stats_one_arr_vre_cens[0],Stats_one_arr_vre_cens[1]+\
                  Stats_one_arr_vre_cens[2],Stats_one_arr_vre_cens[1]-\
-                 Stats_one_arr_vre_cens[2],color='r',alpha=0.4)
+                 Stats_one_arr_vre_cens[2],color='r',alpha=0.4,edgecolor='k')
 plt.xlabel(r'$log_{10}\ R_{e}\ $[kpc]')
 plt.ylabel(r'$log_{10}\ v\ $[km/s]')
 plt.legend(loc='best')
